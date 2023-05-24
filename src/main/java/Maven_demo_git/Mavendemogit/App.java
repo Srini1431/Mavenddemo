8f6f9cfa7304e8044aac64922aec11ba0aab6272
Srini1431 <selenium1.testing1@gmail.com>
2023-05-24 06:37:59 +0100

    login test case

---
+++ src/main/java/Maven_demo_git/Mavendemogit/App.java
@@ -0,0 +1,12 @@
+package Maven_demo_git.Mavendemogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(proxyBeanMethods = false)
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/test/java/Maven_demo_git/Mavendemogit/AppTest.java
@@ -0,0 +1,12 @@
+package Maven_demo_git.Mavendemogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
